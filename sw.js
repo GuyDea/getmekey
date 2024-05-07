dc4cd6fbdeb33f45c4f4658382998744c42cc8a9
GuyDea <theguydea@gmail.com>
2024-05-07 23:28:43 +0200

    don't skip the sw wait

--- sw.js
+++ sw.js
@@ -3,26 +3,35 @@ const URLS_TO_CACHE = [INJECT_ASSETS_TO_PRELOAD];
 
 self.addEventListener('fetch', async (fetchEvent) => {
     if (fetchEvent.request.mode === 'navigate') {
-        fetchEvent.respondWith(
-            (async () => (await caches.open(CACHE_NAME)).match('index.html'))(),
-        )
-    } else {
-        fetchEvent.respondWith(
-            (async () => await (await caches.open(CACHE_NAME))?.match(fetchEvent.request) ?? await fetch(fetchEvent.request))()
-        )
-    }});
+        fetchEvent.respondWith((async () => {
+            try {
+                const preloadResponse = await fetchEvent.preloadResponse;
+                if (preloadResponse) {
+                    return preloadResponse;
+                }
+            } catch (e) {
+            }
+            return (await caches.open(CACHE_NAME)).match('index.html');
+        })(),)
+    } else if (fetchEvent.request.method === 'GET' && URLS_TO_CACHE.some((regex) => fetchEvent.request.url.match(regex))) {
+        fetchEvent.respondWith((async () => await (await caches.open(CACHE_NAME))?.match(fetchEvent.request) ?? await fetch(fetchEvent.request))())
+    }
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil((async () => {
+        if (self.registration.navigationPreload) {
+            await self.registration.navigationPreload.enable();
+        }
+    })());
+});
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    (async () => {
-      const cacheNames = await caches.keys();
-      await Promise.all(
-        cacheNames.map((cacheName) => caches.delete(cacheName))
-      );
-      const cache = await caches.open(CACHE_NAME);
-      await cache.addAll(URLS_TO_CACHE);
-      return self.skipWaiting();
-    })()
-  );
+    event.waitUntil((async () => {
+        const cacheNames = await caches.keys();
+        await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(URLS_TO_CACHE);
+    })());
 });
 
