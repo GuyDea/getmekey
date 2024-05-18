123aecb615d8a20644ffe4fcfab6e51b112dd5da
GuyDea <theguydea@gmail.com>
2024-05-18 22:29:17 +0200

    improve domain name extraction

--- src/services/copy-service.ts
+++ src/services/copy-service.ts
@@ -20,24 +20,53 @@ export class CopyService {
         }
     }
 
+    private _extractMainDomain(url: string) {
+        try {
+            let parsedUrl = new URL(url);
+            let hostname = parsedUrl.hostname;
+
+            // Split the hostname into parts
+            let parts = hostname.split('.');
+
+            // Handle commonly known TLDs with second-level domains
+            let commonSLDs = ['co.uk', 'com.au', 'co.in', 'co.nz', 'co.za', 'com.br'];
+
+            // Check if the last two parts form a common second-level domain
+            if (parts.length > 2) {
+                let lastTwoParts = parts[parts.length - 2] + '.' + parts[parts.length - 1];
+
+                if (commonSLDs.includes(lastTwoParts)) {
+                    // Return the last three parts
+                    return parts.slice(-3)[0];
+                }
+            }
+
+            // Otherwise, return the last two parts for standard TLDs
+            return parts.slice(-2)[0];
+        } catch (error) {
+            console.error("The input is not a valid URL");
+            return null;
+        }
+    }
+
     private _isIpAddress(string: string) {
         return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(string);
     }
 
-    private _getAppNameFromString(string: string | null): string | null {
+    private _getAppNameFromString(string: string | null): string | undefined {
         if(!string){
-            return null;
+            return undefined;
         }
         const tempUrl = string.match(/https?:\/\/.+\./) ? string : `https://${string}`;
         try {
             const hostname = new URL(tempUrl).hostname;
             const parts = hostname.split('.');
             if(parts.length > 1 && !this._isIpAddress(hostname)){
-                return parts.splice(-2)[0].toLowerCase();
+                return this._extractMainDomain(tempUrl)?.toLowerCase();
             }
         } catch (e){
         }
-        return null;
+        return undefined;
     }
 
     private async _tryAppNameInit(){
